2d782173a079802dedb055de23d251fb9ea096df
nikhilcad <nikhilcaddilac@gmail.com>
2024-06-01 02:09:06 +0530

    cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="navbar bg-base-100">
+          <a className="btn btn-ghost text-xl">RSS Reader</a>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import Link from "@/node_modules/next/link";
 import React from "react";
-import {use} from "react";
+import { use } from "react";
 import Parser from "rss-parser";
 
 const fetchFeed = async () => {
@@ -10,7 +11,7 @@ const fetchFeed = async () => {
 
   const feed = await parser.parseURL("https://netflixtechblog.com/feed");
 
-  const arr = feed.items;//.map((item: any) => item.title);
+  const arr = feed.items; //.map((item: any) => item.title);
 
   return arr;
 };
@@ -18,15 +19,26 @@ const fetchFeed = async () => {
 const fetchData = fetchFeed();
 
 export default function Home() {
-
   const data = use(fetchData);
   return (
-    <main>
-      RSS Reader
+    <main className="flex flex-col ">
       {data.map((item: any) => {
-        return <p>{item.title} : {item.link}</p>;
+        console.log(Object.keys(item))
+        return (
+          // <Link href={item.link} target="_blank"><button className="btn btn-primary">{item.title}</button></Link>
+          <div className="card bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{item.title}</h2>
+              <p>{item['content:encodedSnippet']}</p>
+            </div>
+          </div>
+        );
       })}
-      <button className="btn btn-primary">Button</button>
     </main>
   );
 }
