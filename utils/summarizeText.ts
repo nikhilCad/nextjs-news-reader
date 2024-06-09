5a74df52ff3ec01a45077ac15b1d55ab7917f6f5
nikhilcad <nikhilcaddilac@gmail.com>
2024-06-09 19:31:32 +0530

    Remove NextJS

--- utils/summarizeText.ts
+++
@@ -1,7 +0,0 @@
-export const getTextEllipsis = (text: string, length:number = 500):string => {
-
-    if (text?.length <= 100) {
-        return text;
-    }
-    return text?.slice(0, length) + '...';
-}
